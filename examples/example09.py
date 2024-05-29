e05535236ce743ec8799a14c8751c0ac1cb99390
Joan Grebol <116028852+jgrebol@users.noreply.github.com>
2024-05-29 16:21:38 +0200

    Add files via upload

--- examples/example09.py
+++ examples/example09.py
@@ -1,5 +1,9 @@
 import esi
 from utils import ints
 
-Smo = ints.read_int()
+Smo = ints.read_int('./example08_nao/')
+print(Smo)
+print(len(Smo))
+print(len(Smo[0]))
+    
 esi.aromaticity(Smo, rings=[7,3,1,2,6,10], mci=True, av1245=True)
