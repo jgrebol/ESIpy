11558ccca81bcef5ada3dbd7a6f81382a21fd73d
Joan Grebol <116028852+jgrebol@users.noreply.github.com>
2024-06-10 11:08:58 +0200

    Update example02.py

--- examples/example02.py
+++ examples/example02.py
@@ -4,5 +4,5 @@ partition = ['mulliken', 'lowdin', 'meta_lowdin', 'nao', 'iao']
 
 for part in partition:
     Smo = 'example01_' + part + '.aoms'
-    molinfo = 'example01_' + part + '.molinfo
+    molinfo = 'example01_' + part + '.molinfo'
     esi.aromaticity(Smo, rings=[7,3,1,2,6,10], molinfo=molinfo, mci=True, av1245=True, partition=part)
