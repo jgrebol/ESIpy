e986658f285ec67b5be7fc3ef018440553c522d7
jgrebol <joangrebol@gmail.com>
2025-02-04 15:11:46 +0100

    RTD - Added documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'ESIpy'
 copyright = '2024, Joan Grèbol-Tomàs, Eduard Matito, Pedro Salvador'
