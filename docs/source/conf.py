e841b71951336cbeeb24f0d2471943a75089217a
jgrebol <joangrebol@gmail.com>
2025-02-04 16:43:16 +0100

    RTD - Added documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,9 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath("./"))
+sys.path.insert(0, os.path.abspath("../../"))
 
 def setup(app):
     app.add_css_file("css/custom.css")
