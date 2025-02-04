ca3f86fda59c49f7d9f8bec8fdb2041caf1dbb93
jgrebol <joangrebol@gmail.com>
2025-02-04 13:11:44 +0100

    RTD - Added documentation

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,8 @@ mathjax_path="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-M
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'insegel'
-html_static_path = ['../_static']
-html_favicon = '../_static/favicon.ico'
+html_static_path = ['_static']
+html_favicon = 'docs/_static/favicon.ico'
 html_css_files = ["css/custom.css"]
 html_logo = "logoesipy.png"
 html_title = "ESIpy"
