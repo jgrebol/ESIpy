782aabc714a8d3ac5df6011b5c3a1bc9608c66f5
jgrebol <joangrebol@gmail.com>
2025-04-17 17:05:21 +0200

    Read AIMAll AOMs from Restricted, natural wfs + Solved bugs

--- esipy/__init__.py
+++ esipy/__init__.py
@@ -1147,8 +1147,6 @@ class ESI:
         #    raise ValueError(" | IAO and Natural Orbitals not implemented yet")
 
         wf = wf_type(self.aom)
-        print(wf)
-        exit()
         if isinstance(self.rings[0], int):
             self.rings = [self.rings]
         if wf == "rest":
